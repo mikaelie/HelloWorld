54fb8b109cd13c3687b899bee108f6b3242203de
Mikaelie <modom3@asu.edu>
2016-03-20 20:56:42 -0700

    Adding hello.cs

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Main
+{
+	class Hi
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Hello World.\n");
+			Console.Write ("Have a great day!");
+		}
+	}
+}
